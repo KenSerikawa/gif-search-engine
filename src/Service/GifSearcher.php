0b7ab7a1365568351a5ec99651ab25a9377ff7a5
KenSerikawa <kenserikawalucio@gmail.com>
2020-08-03 01:05:42 +0200

    Remove compromised/exposed giphy API KEY

--- src/Service/GifSearcher.php
+++ src/Service/GifSearcher.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+
 final class GifSearcher 
 {
+    private $parameters;
+
+    public function __construct(ContainerBagInterface $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
     public function __invoke($query)
     {
-        $url = "http://api.giphy.com/v1/gifs/search?q=" . $query . "&api_key=FLbs8nIbeKunlATbPPW2nq2wEYVQ3kw9&limit=50";
+        $apiKey = $this->parameters->get('giphy.api.key');
+        $url = "http://api.giphy.com/v1/gifs/search?q=" . $query . "&api_key=" . $apiKey . "&limit=50";
         return json_decode(file_get_contents($url), true);
     }
 }
