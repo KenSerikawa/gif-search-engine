287fa68ce8a50c4df6da374fbf402c6af36db355
KenSerikawa <kenserikawalucio@gmail.com>
2020-08-03 18:25:37 +0200

    Improve styles properties

--- public/js/onLoadSearch.js
+++ public/js/onLoadSearch.js
@@ -3,5 +3,6 @@ function toggleContent() {
     let preloader = document.querySelector('#preloaderBlock')
     list.classList.remove('d-none')
     preloader.classList.add('d-none')
+    list.classList.remove('d-block')
 }
 window.onload = toggleContent;
